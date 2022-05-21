b3c2c523ea1663ca517fc4f6877afc8cdd1c8602
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-05-21 06:48:50 -0700

    completed assignment

--- src/TestComponent.js
+++ src/TestComponent.js
@@ -4,9 +4,7 @@ import React, { Component } from 'react';
 class TestComponent extends Component {
   render() {
     return (
-      <div className="video">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/5TbUxGZtwGI" frameBorder="0" allow="autoplay; encrypted-media" title="time video" allowFullScreen></iframe>
-      </div>
+      Now
     );
   }
 }
